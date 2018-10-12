c94d2f53d5cf2f0e5885111854773776974303a2
jringenbach <ringenbach.julien@gmail.com>
2018-10-12 14:35:20 +0200

    Method loadingLevelElements is complete

--- element.py
+++ element.py
@@ -3,6 +3,6 @@
 class Element:
     """An element is a character, a pnj or a block """
 
-    def __init__(self, skin, isDangerous):
+    def __init__(self, skin, name):
+        self.name = name
         self.skin = skin
-        self.isDangerous = isDangerous
--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import csv
+from element import Element
 
 class Level:
     """Class containing all the attributes for a level """
@@ -8,6 +9,7 @@ class Level:
     def __init__(self, numLevel):
         self.numLevel = numLevel
         self._grilleCSV = list()
+        self._elements = list()
 
         #We search the corresponding csv file depending on the level
         with open("resources/levelFiles.txt", "r") as fileRead:
@@ -37,24 +39,57 @@ class Level:
                 rowSplitted = row[0].split(";")
                 self._grilleCSV.append(rowSplitted)
 
+    def _get_elements(self):
+        return self._elements
+
+    def _set_elements(self, elements):
+        self._elements = elements
+
+        
+    elements = property(_get_elements, _set_elements)
     grilleCSV = property(_get_grille_csv, _set_grille_csv)
 
 #------------------------------------------------------------------------
 #                               METHODS
 #------------------------------------------------------------------------
 
+    def loadingLevelElements(self, listElements):
+        """We create a list of element objects depending on which
+        element is in the level"""
+
+        listElementObjects = list()
+
+        #We gonna check for each element their matching properties in elementsFiles
+        for element in listElements:
+            with open("resources/elementsFiles.txt", "r") as elementFile:
+                for row in elementFile:
+                    rowSplitted = row.split(":")
+
+                    if rowSplitted[0] == element: #rowSplitted[0] is the symbol of the element
+                        elementSkin = rowSplitted[1] #rowSplitted[1] is the path to get its skin
+                        elementName = rowSplitted[2] #rowSplitted[2] is the name of the element
+                        elementToCreate = Element(elementSkin, elementName)
+                        listElementObjects.append(elementToCreate)
+
+        self._set_elements(listElementObjects)
+                        
+                        
     def whichElementIsInTheLevel(self):
         """We search in _grilleCSV which element is present in the level.
         And we return a list of those elements."""
 
         listElements = list()
-        
+
+        #We read each line
         for row in self._get_grille_csv():
+            #We read each cell of each line
             for cell in row:
                 if cell in listElements or cell == "":
                     pass
                 
                 else:
                     listElements.append(cell)
-        print(listElements)
+
+        self.loadingLevelElements(listElements)
+
                 
